e0b62e779dbc3ab56aa3c8c9c86ae2aacb65dd14
hellolimi <hellolaimmn@gmail.com>
2021-05-29 19:03:43 +0900

    Seperate data from the component
    Load JSON data in Home.js and inherit the page data to List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,38 +1,12 @@
-import React, {useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Post from './Post';
 
-function List() {
-    const [data, setData] = useState([]);
-    const [view, setView] = useState(5);
-
-    const viewmore = () => {
-        setView(prev => prev += 5);
-    }
-   
-    const onDelete = postId => {
-        setData(prev => prev.filter(el => el.id !== postId));
-        setView(prev => prev += 1);
-    }
-
-    useEffect(()=>{
-        const fetchData = async () => {
-            let dataUrl = "https://jsonplaceholder.typicode.com/albums";
-            try{
-                const response = await axios.get(dataUrl);
-                setData(response.data);
-            }catch (e){
-                throw Error(e);
-            }
-        }
-        fetchData();
-    }, []);
+function List({data, onDelete}) {
 
     return <div>
         <ul>
-            {data.map(post => post.id<=view&&<Post key={post.id} postId={post.id} postTitle={post.title} onDelete={onDelete} />)}
+            {data.map(post => <Post key={post.id} postId={post.id} postTitle={post.title} onDelete={onDelete} />)}
         </ul>
-        <button type="button" onClick={viewmore}>VIEW MORE</button>
     </div>
 }
 
