c7088fe553a98e521cf3a52d1d4476881aabc723
hellolimi <hellolaimmn@gmail.com>
2021-05-29 16:13:29 +0900

    Set up Router
    Redriect users to Home once they're signed in

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
 
 function AppRouter() {
-    return <div>
-            
-    </div>
+    const [userIn, setUserIn] = useState(false);
+
+    useEffect(()=>{
+        if(sessionStorage.getItem('token')){
+            setUserIn(true);
+        }else{
+            setUserIn(false);
+        }
+    }, []);
+    return <Router>
+        <Switch>
+            {userIn?
+                <Route exact path="/">
+                    <Home />
+                </Route>:
+                <Route exact path="/"> 
+                    <Auth />
+                </Route>
+            }
+        </Switch>
+    </Router>
 }
 
 export default AppRouter;
