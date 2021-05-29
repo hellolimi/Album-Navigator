25714b2c39462651d6cc056023427929b74d16f6
hellolimi <hellolaimmn@gmail.com>
2021-05-30 01:01:17 +0900

    Add edit toggle & style
    Create the edit button to toggle update/delte box
    Add up style to the component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
+
+const PostBlock = styled.li`
+    position: relative; 
+    padding:2.5rem 0; border-bottom:1px solid #FF9E9E;
+    .editBox { position:absolute; top:2.5rem; right:0; width:3.5rem;
+        > button { float:right; margin-bottom:0.5rem; border:none; background-color:transparent; color:#FF9E9E; }
+        > div{ float:right; display:flex; flex-direction:column; background-color:#FF9E9E;
+            button{ padding:1rem; border:none; background-color:transparent; color:#fff; font-weight:bold; }
+        }
+    }
+    h4{ width:100%; margin-bottom:1.25rem; text-align:center; font-size:2rem; font-weight:normal; }
+    img{ margin:0 auto; }
+
+    form{ text-align:center;  
+        fieldset{ margin-bottom:1rem; > * { height:3rem; }
+            input{ width:40%; margin-right:1rem; padding:0 0.5rem ; border:1px solid #ededed; }
+            button{ width:15%; background-color:#FF9E9E; border:none; border-radius:0.5rem; color:#fff; }
+        }
+    }
+`;
 
 function Post({postId, postTitle, onDelete, onUpdate}) {
     const [update, setUpdate] = useState(false);
     const [newTitle, setNewTitle] = useState(postTitle);
+    const [edit, setEdit] = useState(false);
 
-    const onToggle = () => {
-        setUpdate(prev => !prev);
+    const onToggle = e => {
+        const {name} = e.target;
+        console.log(name);
+        if(name === "edit"){
+            setEdit(prev => !prev);
+        }else{
+            setUpdate(prev => !prev);
+        }
     }
     
     const onChange = e => {
@@ -18,17 +46,27 @@ function Post({postId, postTitle, onDelete, onUpdate}) {
         if(newTitle !== ''){
             onUpdate(newTitle, postId);
             setUpdate(false);
+            setEdit(false);
         }
     }
     
     return (
-        <li>
-            <button onClick={onToggle}>Update</button>
-            <button onClick={()=>{onDelete(postId)}} >Delete</button>
+        <PostBlock>
+            <div className="editBox">
+                <button type="button" name="edit" onClick={onToggle}>● ● ●</button>
+                {edit&&
+                    <div>
+                        <button type="button" name="update" onClick={onToggle}>Update</button>
+                        <button type="button" onClick={()=>{onDelete(postId)}} >Delete</button>
+                    </div>}
+            </div>
             {update?
                 <form onSubmit={onSubmit}>
-                    <input type="text" value={newTitle} onChange={onChange}/>
-                    <button type="submit">Submit</button>
+                    <fieldset>
+                        <input type="text" value={newTitle} onChange={onChange}/>
+                        <button type="submit">Update!</button>
+                    </fieldset>
+                    <img src="http://placehold.it/580x400/cccccc/ffffff?text=demo-image" alt="demo" />
                 </form>:
                 <div>
                     <h4>{postTitle}</h4>
@@ -36,7 +74,7 @@ function Post({postId, postTitle, onDelete, onUpdate}) {
                 </div>
             }
             
-        </li>
+        </PostBlock>
     );
 }
 
