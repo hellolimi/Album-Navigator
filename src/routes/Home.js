581328e5cc355d80867037be050dff37ed6fd960
hellolimi <hellolaimmn@gmail.com>
2021-05-29 19:06:26 +0900

    Get data in Home.js & Add pagination components
    Set up onDelete function in Home.js // where the raw data exist
    Create getPost function to slice data for 5 posts for each page
    Add pagination components

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import List from '../components/List';
+import Pagination from '../components/Pagination';
 
 function Home(props) {
-    
+    const [data, setData] = useState([]);
+    const [currentPg, setCurrentPg] = useState(1);
+    const [postsToView, setpostsToView] = useState(5);
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            let dataUrl = "https://jsonplaceholder.typicode.com/albums";
+            try{
+                const response = await axios.get(dataUrl);
+                setData(response.data);
+            }catch (e){
+                throw Error(e);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const getPosts = viewData => {
+        const lastIndex = currentPg * postsToView;
+        const firstIndex = lastIndex - postsToView;
+        
+        let currentPost = viewData.slice(firstIndex, lastIndex);
+        return currentPost;
+    }
+
+    const onDelete = postId => {
+        setData(prev => prev.filter(el => el.id !== postId));
+    } 
+
 
     return <div>
-        <List />
+        <List data={getPosts(data)} onDelete={onDelete} />
+        <Pagination postsToView={postsToView} totalPosts={data.length} paginate={setCurrentPg} />
     </div>
 }
 
