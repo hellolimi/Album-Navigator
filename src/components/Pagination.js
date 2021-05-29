fe5d043ccd24bdadb790f4307411f3a87d61f8f7
hellolimi <hellolaimmn@gmail.com>
2021-05-30 01:38:00 +0900

    Create prev/next button for paginations
    To show 5 buttons each time

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,24 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
+
+const PaginationBlock = styled.div`
+    * { color:#FF9E9E; font-weight:bold; font-size:1.5rem; }
+    display:flex; justify-content:center; padding:2rem;
+    ul{ position:relative; top:3px; display:flex; 
+        li{ width:2.5rem; height:2.5rem; 
+            button { width:100%; height:100%; border:none; background-color:transparent;  }
+        }
+    }
+    button { width:2.5rem; height:2.5rem; border:none; background-color:transparent;  }
+    
+`;
 
 function Pagination({postsToView, totalPosts, paginate}) {
 
     const [pageNums, SetpageNums] = useState([]);
+    const [currentBtn, setCurrentBtn] = useState({
+        start: 1,
+        end: 5
+    });
+    
+    const prevNextBtn = e => {
+        const {name} = e.target;
+        const {start, end} = currentBtn;
+        const lastPage = Math.ceil(totalPosts/postsToView);
+        if(name === "next"){
+            if(end < lastPage){
+                setCurrentBtn(prev=> ({start: prev.start++, end: prev.end++}))
+            }
+        }else{
+            if(start > 1){
+                setCurrentBtn(prev=> ({start: prev.start--, end: prev.end--}))
+            }
+        }
+    }
 
     useEffect(()=>{
         const setPagination = () => {
-            for(let i = 1; i<=Math.ceil(totalPosts/postsToView);i++){
-                SetpageNums(prev=> [...prev, i]);
+            const {start, end} = currentBtn;
+            let numArray = [];
+            for(let i = start; i<=end;i++){
+                numArray.push(i);
             }
+            SetpageNums(numArray);
         }
 
         setPagination();
-    }, [totalPosts, postsToView]);
+    }, [totalPosts, postsToView, currentBtn]);
 
     return (
-        <ul>
-            {pageNums.map(num => <li key={uuidv4()}><button onClick={()=>{paginate(num)}}>{num}</button></li>)}
-        </ul>
+        <PaginationBlock>
+            <button type="button" name="prev" onClick={prevNextBtn}>◀</button>
+            <ul>
+                {pageNums.map(num => <li key={uuidv4()}><button onClick={()=>{paginate(num)}}>{num}</button></li>)}
+            </ul>
+            <button type="button" name="next" onClick={prevNextBtn}>▶</button>
+        </PaginationBlock>
     );
 }
 
